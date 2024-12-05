c8df1cdf91479a6080a26f1722c16b13a2a768b5
deedly001 <ariasmall@yandex.ru>
2024-12-05 13:22:34 +0300

    /

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -8,7 +8,7 @@ public class App {
         products.setProductInBasket(new Product("pineapple", 240));
         products.setProductInBasket(new Product("watermelon", 400));
         products.setProductInBasket(new Product("mango", 360));
-//        products.getAllCost();
+        products.getAllCost();
         products.printAllBasket();
 //        products.setProductInBasket(new Product("apples", 300));
 //        products.printAllBasket();

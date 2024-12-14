4b609dac0d34123ab5761c766f4a2ceae1698e1c
deedly001 <ariasmall@yandex.ru>
2024-12-14 15:07:34 +0300

    /

---
+++ src/org/skypro/skyshop/FixPriceProduct.java
@@ -0,0 +1,24 @@
+package org.skypro.skyshop;
+
+public class FixPriceProduct extends Product {
+    private final Integer fixPrice = getPrice();
+    private String productName;
+
+    public FixPriceProduct(String productName) {
+        super(productName);
+    }
+
+    public Integer getPrice() {
+        return fixPrice;
+    }
+
+    @Override
+    public boolean isSpecial() {
+        return false;
+    }
+
+//    @Override
+//    public String toString() {
+//        return "<" + productName + "> : <Фиксированная цена: " + fixPrice + ">" ;
+//    }
+}
---
+++ src/org/skypro/skyshop/SimpleProduct.java
@@ -0,0 +1,28 @@
+package org.skypro.skyshop;
+
+public class SimpleProduct extends Product {
+    private final Integer productCost;
+    private String productName;
+    private boolean isSpecial = false;
+
+    public SimpleProduct(String productName, Integer productCost) {
+        super(productName);
+        this.productCost = productCost;
+    }
+
+    @Override
+    public Integer getPrice() {
+        return productCost;
+    }
+
+    @Override
+    public boolean isSpecial() {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "<" + productName + "> : <" + productCost + ">" ;
+    }
+
+}

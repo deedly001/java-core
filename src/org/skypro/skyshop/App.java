1d290d9d5f1c8b06dfabedc54dfdebf26a56f5f8
deedly001 <ariasmall@yandex.ru>
2024-12-21 19:57:53 +0300

    /

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -5,21 +5,21 @@ import org.skypro.skyshop.product.FixPriceProduct;
 import org.skypro.skyshop.product.SimpleProduct;
 
 public class App {
-    public static void main(String[] args) {
-        ProductBasket products = new ProductBasket();
+    public void main(String[] args) {
+//        ProductBasket products = new ProductBasket();
 //        System.out.println(" -> Демонстрация классов");
 //        System.out.println(" -> Добавление продукта в корзину");
-        products.setProductInBasket(new SimpleProduct("apple", 300));
-        products.setProductInBasket(new SimpleProduct("orange", 250));
-        products.setProductInBasket(new SimpleProduct("pineapple", 240));
+//        products.setProductInBasket(new SimpleProduct("apple", 300));
+//        products.setProductInBasket(new SimpleProduct("orange", 250));
+//        products.setProductInBasket(new SimpleProduct("pineapple", 240));
 //        products.setProductInBasket(new SimpleProduct("watermelon", 400));
 //        products.setProductInBasket(new SimpleProduct("mango", 360));
 //        System.out.println(" -> Добавление продукта в заполненную корзину, в которой нет свободного места");
 //        System.out.println(" -> Печать содержимого корзины с несколькими товарами.");
 //        products.printAllBasket();
-        products.getDiscountedProduct(new DiscountedProduct("mango", 360, 5));
-        products.getFixPriceProduct(new FixPriceProduct("Watermelon"));
-        products.printAllBasket();
+//        products.getDiscountedProduct(new DiscountedProduct("mango", 360, 5));
+//        products.getFixPriceProduct(new FixPriceProduct("Watermelon"));
+//        products.printAllBasket();
 //        System.out.println(" -> Получение стоимости корзины с несколькими товарами.");
 //        products.getAllCost();
 //        System.out.println(" -> Поиск товара, который есть в корзине.");
--- src/org/skypro/skyshop/Article.java
+++ src/org/skypro/skyshop/Article.java
@@ -1,6 +1,6 @@
 package org.skypro.skyshop;
 
-public abstract class Article {
+public final class Article {
     private String article;
     private String articleText;
 
@@ -8,18 +8,31 @@ public abstract class Article {
     public String toString() {
         return article + "\n" + articleText;
     }
+
+    public String getArticleName(){
+        return this.article;
+    }
+
+    public String getArticleText(){
+        return "ARTICLE";
+    }
+
+    public String getSearchTerm(){
+        return "name: "+ getArticleName() + " type: " + getArticleText();
+    }
 }
 
 interface Searchable{
-    public default String getSearchTerm(){
-        return null;
-    }
+//    public default String getSearchTerm(String text){
+//        return text;
+//    }
+//
+//    public default String getContentType(){
+//        return null;
+//    }
+//
+//    public default void getStringRepresentation(){
+//        System.out.println("«имя: "+ getSearchTerm() +" — тип: " + getContentType());
+//    }
 
-    public  default String getContentType(){
-        return null;
-    }
-
-    public  default String getStringRepresentation(){
-        return "«имя Searchable-объекта — тип Searchable-объекта»";
-    }
 }

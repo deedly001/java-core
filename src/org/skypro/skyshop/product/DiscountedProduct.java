c18cfd22fad16bc005ff1cad5ce6dc90c396a715
deedly001 <ariasmall@yandex.ru>
2025-01-10 20:32:44 +0300

    /

--- src/org/skypro/skyshop/product/DiscountedProduct.java
+++ src/org/skypro/skyshop/product/DiscountedProduct.java
@@ -15,11 +15,11 @@ public class DiscountedProduct extends Product {
 
     private void checkDiscountPrice(Integer discountPrice) throws IllegalArgumentException {
         if (basePrice != null) {
-            if (discountPrice < 0 || discountPrice > 100) {
+            if (discountPrice == 0) {
+                throw new NullPointerException(("Скидка не может быть = 'Null'"));
+            } else if (discountPrice < 0 || discountPrice > 100) {
                 throw new IllegalArgumentException(("Скидка на продукт: " + this.getProductName() + " не может быть менее 0% и более 100%"));
             }
-        } else {
-            throw new NullPointerException(("Скидка не может быть = 'Null'"));
         }
     }
 
@@ -27,15 +27,16 @@ public class DiscountedProduct extends Product {
         if (basePrice != null) {
             if (basePrice < 1) {
                 throw new IllegalArgumentException(("Цена продукта: \"" + basePrice + "\"" + " должна быть больше чем = 0"));
+            } else if (basePrice == 0) {
+                throw new NullPointerException(("Цена продукта не может быть = 'Null'"));
             }
-        } else {
-            throw new NullPointerException(("Цена продукта не может быть = 'Null'"));
         }
     }
 
+
     @Override
     public Integer getPrice() {
-        return (basePrice - (discountPrice * basePrice) / 100);
+        return (int) (basePrice * (1 - discountPrice / 100f));
     }
 
     @Override

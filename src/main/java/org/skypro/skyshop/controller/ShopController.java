f8e247ae723e4813b3b9843e5216bf825adebf59
deedly001 <ariasmall@yandex.ru>
2025-02-23 14:14:50 +0300

    /

--- src/main/java/org/skypro/skyshop/controller/ShopController.java
+++ src/main/java/org/skypro/skyshop/controller/ShopController.java
@@ -8,15 +8,13 @@ import org.skypro.skyshop.service.BasketService;
 import org.skypro.skyshop.service.SearchService;
 import org.skypro.skyshop.service.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.UUID;
 
 @RestController
+@RequestMapping("/shop")
 public class ShopController {
     private final StorageService storageService;
     private final SearchService searchService;
